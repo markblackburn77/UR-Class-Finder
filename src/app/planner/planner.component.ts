9bb5cc348e3af88fea5aed98dfe41b0aff895771
Jack Ditto <jackditto7@gmail.com>
2019-05-13 22:12:36 -0500

    Create new planner component

---
+++ src/app/planner/planner.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { ClassesService } from "../shared/classes.service";
+import { Class } from "../shared/class.model";
+
+@Component({
+  selector: "app-planner",
+  templateUrl: "./planner.component.html",
+  styleUrls: ["./planner.component.scss"]
+})
+export class PlannerComponent implements OnInit {
+  /** Prospective classes */
+  cart: Class[];
+
+  // Inject classesService
+  constructor(private classesService: ClassesService) {}
+
+  ngOnInit() {
+    // Set the data in this component to master prospective classes list
+    this.cart = this.classesService.getProspectiveClasses();
+  }
+
+  /**
+   * Removes a class from the cart and updates the data in the component accordingly.
+   *
+   * @param c Class to remove from cart.
+   */
+  removeFromCart(c: Class) {
+    this.classesService.removeFromCart(c);
+    this.cart = this.classesService.getProspectiveClasses();
+  }
+}
