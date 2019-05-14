2fa118d9dd14b5ef5c404e016f2c32e59981a776
Jack Ditto <jackditto7@gmail.com>
2019-05-13 22:14:53 -0500

    Remove duplicate import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { MultiselectDropdownModule } from "angular-2-dropdown-multiselect";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
-import { ClassesService } from "./search/classes.service";
 import { ClassesService } from "./shared/classes.service";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { HttpModule, JsonpModule } from "@angular/http";
