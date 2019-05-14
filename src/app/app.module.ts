83b7ade9e7e41a3423f1f6d937b2ef4fb6ecfeaf
Jack Ditto <jackditto7@gmail.com>
2019-05-13 22:11:25 -0500

    Remove useless header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,12 @@ import { ClassesService } from "./shared/classes.service";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { HttpModule, JsonpModule } from "@angular/http";
 import { HttpService } from "./search/http.service";
-import { HeaderComponent } from './header/header.component';
 import { PlannerComponent } from "./planner/planner.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { TableComponent } from "./table/table.component";
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, DropdownDirective, HeaderComponent],
   imports: [
     BrowserModule,
     MultiselectDropdownModule,
