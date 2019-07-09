edaea7b55a1539ac05a6b7ee023cc6093159bb52
Jack Ditto <jackditto7@gmail.com>
2019-07-08 22:18:10 -0500

    Change some variable names for clarity and minor code optimizations

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -40,8 +40,8 @@ export class SearchComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // Set classes to classes stored in classes service
-    this.classes = this.classesService.getClasses();
+    // Set classes to classes loaded from search
+    this.classes = this.classesService.getClassesLoadedFromSearch();
   }
 
   /**
@@ -49,18 +49,17 @@ export class SearchComponent implements OnInit {
    *
    * @param formData form data from template driven form
    */
-  getClassesData(formData) {
-    this.httpService.sendData(formData).subscribe(
+  searchClasses(formData) {
+    this.httpService.sendSearchData(formData).subscribe(
       // Add classes in classes service if successful
       response => {
-        this.classesService.clearClasses();
-        this.classesService.addClassesFromResponse(response);
+        this.classesService.loadClassesFromSearch(response);
       },
       // Change variable for popup if error
       error => (this.serverUnreacheable = true),
-      // If completed, render classes and clear all from previous search
+      // If completed, render classes
       () => {
-        this.classes = this.classesService.getClasses();
+        this.classes = this.classesService.getClassesLoadedFromSearch();
       }
     );
   }
@@ -87,33 +86,23 @@ export class SearchComponent implements OnInit {
    * Runs on form submission, sends correct data from forms to API
    */
   onSubmit() {
-    // Assign variables from form
-    this.className = this.searchForm.value.className;
-    this.crn = this.searchForm.value.crn;
-    this.professor = this.searchForm.value.professor;
-
     // Format the data a little
-    let formatted_data = {
-      class_name: this.className,
-      crn: this.crn,
-      professor: this.professor,
+    const formatted_data = {
+      class_name: this.searchForm.value.className,
+      crn: this.searchForm.value.crn,
+      professor: this.searchForm.value.professor,
       department: this.dropdownSelectedValues
     };
 
     // Send request to server
-    this.getClassesData(formatted_data);
-  }
-
-  // onChange mostly for testing right now
-  onChange() {
-    console.log(this.dropdownSelectedValues);
+    this.searchClasses(formatted_data);
   }
 
   /**
    * Populate table with test classes to demo app without backend running
    */
   onClickTestData() {
-    this.classesService.setClasses([
+    this.classesService.setClassesLoadedFromSearch([
       new Class(
         'Computer Science',
         1234,
@@ -155,6 +144,6 @@ export class SearchComponent implements OnInit {
         'FYS'
       )
     ]);
-    this.classes = this.classesService.getClasses();
+    this.classes = this.classesService.getClassesLoadedFromSearch();
   }
 }
