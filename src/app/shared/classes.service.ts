7cd78c62964d1137f1721e271c58e1dd6b762b2b
Jack Ditto <jackditto7@gmail.com>
2019-07-08 22:20:57 -0500

    Refactor classes service for better logic and variable names

--- src/app/shared/classes.service.ts
+++ src/app/shared/classes.service.ts
@@ -1,124 +1,37 @@
-import { Class } from "./class.model";
-import { MeetingTime } from "./meeting-time.model";
+import { Class } from './class.model';
+import { MeetingTime } from './meeting-time.model';
 
 export class ClassesService {
-  /** All loaded classes */
-  private classes: Class[] = [];
+  /** All classes loaded from the last search */
+  private loadedClassesFromSearch: Class[] = [];
 
-  /** Classes in the "cart" */
-  public prospectiveClasses: Class[] = [];
+  /** Classes added to the cart */
+  public currentCart: Class[] = [];
+
+  /** All sections of classes in the cart */
+  private allSectionsOfCurrentCart: Class[] = [];
+
+  // Accessor Methods
 
   /**
    * Returns a copy of the class list (but not the actual list)
    */
-  getClasses() {
-    return this.classes.slice();
-  }
-
-  getProspectiveClasses() {
-    return this.prospectiveClasses.slice();
+  getClassesLoadedFromSearch(): Class[] {
+    return this.loadedClassesFromSearch.slice();
   }
 
   /**
-   * Add a class to the cart
-   *
-   * @param index of the class object within the classes list
+   * Returns a copy of the classes cart (but not the actual list)
    */
-  addToCart(c: Class) {
-    // Change variable in class to true
-    c.inCart = true;
-    // Add copy of class to cart
-    this.prospectiveClasses.push(c);
+  getCurrentCart(): Class[] {
+    return this.currentCart.slice();
   }
 
   /**
-   * Remove a class from the cart
-   *
-   * @param index of the class object within the classes list
+   * Returns a copy of all the sections of classes in the current cart (but not the actual list)
    */
-  removeFromCart(c: Class) {
-    // Toggle the class being in a cart
-    c.inCart = false;
-    // Remove the class from the cart
-    for (var i = 0; i < this.prospectiveClasses.length; i++) {
-      if (c.crn == this.prospectiveClasses[i].crn) {
-        this.prospectiveClasses.splice(i, 1);
-      }
-    }
-  }
-
-  /**
-   * Clear all of the loaded classes
-   */
-  clearClasses() {
-    this.classes = [];
-  }
-
-  /**
-   * Set the list of classes to a new list
-   *
-   * @param c new list of class objects
-   */
-  setClasses(c: Class[]) {
-    this.classes = c;
-  }
-
-  /**
-   * Parse JSON response from API into class objects
-   *
-   * @param response JSON response from API
-   */
-  addClassesFromResponse(response): void {
-    // Iterate through classes returned and add as objects
-    for (let i = 0; i < response.length; i++) {
-      // Create new class object
-      let newClass: Class = new Class(
-        response[i]["class_name"],
-        response[i]["crn"],
-        response[i]["professor"],
-        [],
-        response[i]["building"],
-        response[i]["department"]
-      );
-
-      if (
-        response[i]["start_time"] &&
-        response[i]["end_time"] &&
-        response[i]["week_code"]
-      ) {
-        newClass.meetingTimes.push(
-          new MeetingTime(
-            response[i]["start_time"],
-            response[i]["end_time"],
-            response[i]["week_code"]
-          )
-        );
-      }
-
-      this.addClass(newClass);
-    }
-  }
-
-  /**
-   * Add a class to the list of classes. Also checks if a duplicate
-   * exists or if the class is currently in the cart ("prospective classes")
-   *
-   * @param c new class object
-   */
-  private addClass(c: Class) {
-    // Check for duplicates already in classes list
-    let duplicates: Class[] = this.classes.filter(x => x.crn == c.crn);
-    if (duplicates.length > 0) {
-      duplicates[0].meetingTimes = duplicates[0].meetingTimes.concat(
-        c.meetingTimes
-      );
-    } else {
-      // Check if class is in cart
-      if (this.classInCart(c)) {
-        c.inCart = true;
-      }
-      this.classes.push(c);
-    }
+  getAllSectionsOfCurrentCart(): Class[] {
+    return this.allSectionsOfCurrentCart.slice();
   }
 
   /**
@@ -127,11 +40,140 @@ export class ClassesService {
    * @param c Class object
    */
   private classInCart(c: Class): boolean {
-    for (var i = 0; i < this.prospectiveClasses.length; i++) {
-      if (c.crn == this.prospectiveClasses[i].crn) {
+    for (let i = 0; i < this.currentCart.length; i++) {
+      if (c.crn === this.currentCart[i].crn) {
         return true;
       }
     }
     return false;
+  }
+
+  // Mutator methods
+
+  /**
+   * Set the list of classes loaded from the last search to a new list
+   *
+   * @param c new list of class objects
+   */
+  setClassesLoadedFromSearch(c: Class[]) {
+    this.loadedClassesFromSearch = c;
+  }
+
+  /**
+   * Clear all of the classes loaded from the last search
+   */
+  clearClassesLoadedFromSearch() {
+    this.loadedClassesFromSearch = [];
+  }
+
+  /**
+   * Add a class to the list of classes loaded from the search.
+   *
+   * @param c new class object
+   */
+  public addToLoadedClassesFromSearch(c: Class) {
+    // Check if class is in cart
+    if (this.classInCart(c)) {
+      c.inCart = true;
+    }
+    this.loadedClassesFromSearch.push(c);
+  }
+
+  /**
+   * Add a class to the cart
+   *
+   * @param index of the class object within the classes list
+   */
+  addToCart(c: Class): void {
+    c.inCart = true;
+    this.currentCart.push(c);
+  }
+
+  /**
+   * Remove a class from the cart
+   *
+   * @param c class object to remove
+   */
+  removeFromCart(c: Class) {
+    c.inCart = false;
+
+    for (var i = 0; i < this.currentCart.length; i++) {
+      if (c.crn == this.currentCart[i].crn) {
+        this.currentCart.splice(i, 1);
+      }
+    }
+  }
+
+  /**
+   * Load classes from the search
+   *
+   * @param classesDataFromServer JSON data returned from server
+   */
+  public loadClassesFromSearch(classesDataFromServer): void {
+    this.clearClassesLoadedFromSearch();
+    this.loadedClassesFromSearch = this.readClassesFromResponse(
+      classesDataFromServer
+    );
+  }
+
+  public loadAllSectionsOfCart(classesDataFromServer): void {
+    this.allSectionsOfCurrentCart = this.readClassesFromResponse(
+      classesDataFromServer
+    );
+  }
+
+  /**
+   * Takes the JSON response return from the API and parses classes into Class objects
+   *
+   * @param response JSON response from API
+   * @returns array of loaded classes
+   */
+  private readClassesFromResponse(response): Class[] {
+    // Array to store loaded classes
+    let loadedClasses: Class[] = [];
+
+    // Iterate through classes returned and add as objects
+    for (let i = 0; i < response.length; i++) {
+      // Create new class object
+      let newClass: Class = new Class(
+        response[i]['class_name'],
+        response[i]['crn'],
+        response[i]['professor'],
+        [],
+        response[i]['building'],
+        response[i]['department']
+      );
+
+      // Check if the class has meeting time info before adding
+      if (
+        response[i]['start_time'] &&
+        response[i]['end_time'] &&
+        response[i]['week_code']
+      ) {
+        newClass.meetingTimes.push(
+          new MeetingTime(
+            response[i]['start_time'],
+            response[i]['end_time'],
+            response[i]['week_code']
+          )
+        );
+      }
+
+      // Check for duplicates already in classes list
+      let duplicates: Class[] = loadedClasses.filter(
+        x => x.crn == newClass.crn
+      );
+
+      // Add classes meeting times to already existing class
+      // Note: This case should happen if a class has two sections
+      if (duplicates.length > 0) {
+        duplicates[0].meetingTimes = duplicates[0].meetingTimes.concat(
+          newClass.meetingTimes
+        );
+      } else {
+        loadedClasses.push(newClass);
+      }
+    }
+    return loadedClasses;
   }
 }
