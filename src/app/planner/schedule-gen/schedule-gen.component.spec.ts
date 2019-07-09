da8a0685ecd8a78df84386378f2e7134bfe80c57
Jack Ditto <jackditto7@gmail.com>
2019-07-08 22:03:40 -0500

    Implement some basic testing for schedule gen component

--- src/app/planner/schedule-gen/schedule-gen.component.spec.ts
+++ src/app/planner/schedule-gen/schedule-gen.component.spec.ts
@@ -1,16 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ScheduleGenComponent } from './schedule-gen.component';
+import { ScheduleGenComponent } from "./schedule-gen.component";
+import { Class } from "src/app/shared/class.model";
+import { MeetingTime } from "src/app/shared/meeting-time.model";
+import { DummyData } from "./dummy-data";
+import { ClassesService } from "src/app/shared/classes.service";
+import { HttpService } from "src/app/shared/http.service";
+import { Http } from "@angular/http";
 
-describe('ScheduleGenComponent', () => {
+describe("ScheduleGenComponent", () => {
   let component: ScheduleGenComponent;
   let fixture: ComponentFixture<ScheduleGenComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScheduleGenComponent ]
-    })
-    .compileComponents();
+      declarations: [ScheduleGenComponent],
+      providers: [
+        { provide: ClassesService, useClass: ClassesService },
+        { provide: HttpService, useClass: HttpService },
+        { provide: Http }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +29,20 @@ describe('ScheduleGenComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Test isCompatible", () => {
+    let c1: Class = DummyData.classes[3];
+    let c2: Class = DummyData.classes[4];
+
+    expect(component.isCompatible(c1, c2)).toBe(false);
+  });
+
+  it("Test intersection", () => {
+    expect(
+      component.intersection(["M", "W", "F"], ["M", "W", "F"]).length
+    ).toBe(3);
   });
 });
